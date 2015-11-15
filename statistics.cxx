fa4bccb4915b65705234b9b9111827c2a7198b96
Student <martin.boeckmann@uni-duesseldorf.de>
2015-11-15 21:51:33 +0100

    Martins Version. Have fun =)

--- statistics.cxx
+++ statistics.cxx
@@ -3,19 +3,44 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+
+void random(double* pointer, int N){
+	for(int i=0;i<N;i++){
+		*pointer = rand()/(double)RAND_MAX;
+		pointer = pointer+1;	
+	}
+}
+
+void Mittelwert(double& mean, int N, double* pointer){
+		for(int i = 0; i < N ; i++){
+		mean += *(pointer+i); 
+		}
+		mean /= N;
+}
+
+void Varianz(double& var,double& mean, int N, double* pointer){
+		for(int i = 0; i < N ; i++){
+			var += (*(pointer+i)-mean) * (*(pointer+i)-mean ); 
+		}
+		var /= N;
+}
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
+   double* pointer = &p[0];
+   double* helferling = pointer;
+   
+   random(pointer,N);
+   pointer = helferling;
+   Mittelwert(mean,N,pointer);
+   pointer = helferling;
+   Varianz(var,mean,N,pointer);
 
-   // Some lines here....
-
-   cout << "Mean = " << mean << endl;
-   cout << "Variance = " << var << endl;
+	cout << "Mean = " << mean << endl;
+	cout << "Variance = " << var << endl;
 
    return 0;
 }
